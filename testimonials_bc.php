43db0ae85acfc33f56ee0b2ea11be70d57755b37
unknown <Mark@Mark-PC.berea.edu>
2015-11-19 17:17:44 -0500

    Added the reference to the actual page of the testimony.

--- testimonials_bc.php
+++ testimonials_bc.php
@@ -18,10 +18,12 @@ function create_post_type() {
         'singular_name' => __( 'Testimonial' )
       ),
       'public' => true,
+      'publicly_queryable' => true,
       'has_archive' => true,
       'show_ui' => true,
       'show_in_nav_menus' => true,
       'show_in_menu' => true,
+      'capability_type'    => 'page',
       'edit_post'          => 'edit_testimonials',
       'read_post'          => 'read_testimonials',
       'delete_post'        => 'delete_testimonials',
@@ -50,20 +52,30 @@ function testimonial($atts, $content = null){
 
   //r is the result of the query
   $r = $wpdb->get_row($q);
-  //print $r->post_content;
+
+  // get the name of the person
+  //also assumes it's the post title
+  $name = $r->post_title;
+
+  //restricts the testimonials to 100 charachters long
+  $to_post = substr($r->post_content,0,100);
+
+  //return the guid of the page
+  $link = $r->guid;
+
 
   //Query to pull the primary image
   $q = "SELECT * FROM " . $wpdb->posts . " WHERE post_parent = " . $id;
 
   //get the row with the link to the image
   $p = $wpdb->get_row($q);
+  $p_link = $p->guid;
 
   $z = plugins_url( 'testimonial_style.css', __FILE__ );
   wp_register_style( 'testimonial_bc_style', $z );
   wp_enqueue_style('testimonial_bc_style');
 
-  //print out the results of the search
-  return '<div class="testimonial_bc"><img src=' . $p->guid . '>' . $r->post_content . ' </div>';
+  return '<a href="'.$link.'"><div class="testimonial_bc"><img src=' . $p_link . '>' . $to_post . '... Read more. </div></a>';
 }
 add_shortcode("testimonial", "testimonial");
 
